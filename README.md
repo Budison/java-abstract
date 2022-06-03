66f8ca4d9a7d064b179050f5fa7e856287496b39
Kevin Nowak <kevin_nowak@epam.com>
2022-06-03 12:26:27 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# java_abstract
+
+This project was created on 2022-06-03 from a template.
---
+++ src/main/java/com/github/budison/java_abstract/Main.java
@@ -0,0 +1,8 @@
+package com.github.budison.java_abstract;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}	
